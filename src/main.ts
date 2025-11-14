cfb66492730172f54c983b61fcf1c8f51c1393c0
Édouard Roger <edouard.roger+github@gmail.com>
2025-11-14 14:11:21 +0100

    Mise à jour de la feuille de style importée.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import 'dsfr-postale/dist/style.css'
+import 'dsfr-postale/dist/dsfr-postale.css'
 createApp(App).mount('#app')
